c0e3ad02d17f5da75263c49cd57ddf8a87f82c21
Marco Nunnari <mnunnari@student.42.us.org>
2017-04-21 13:49:49 +0200

    seem working

--- get_next_line.c
+++ get_next_line.c
@@ -2,10 +2,35 @@
 
 int			get_next_line(const int fd, char **line)
 {
-	(void)fd;
 	(void)line;
-	char		*buffer;
+	char			*buffer;
+	char			*newlineptr;
+	static char		*remain;
 
 	IFRETURN(!(buffer = ft_strnew(BUFF_SIZE + 1)), -1);
+	*line = "";
+	if (remain)
+	{
+		if ((newlineptr = ft_strchr(remain, '\n')))
+		{
+			*line = ft_strjoin(*line, ft_strsub(remain, 0, newlineptr - remain));
+			remain = newlineptr + 1;
+			return (1);
+		}
+		*line = ft_strjoin(*line, remain);
+	}
+	while ((read(fd, buffer, BUFF_SIZE) > 0))
+	{
+		if ((newlineptr = ft_strchr(buffer, '\n')))
+		{
+			*line = ft_strjoin(*line, ft_strsub(buffer, 0, newlineptr - buffer));
+			remain = newlineptr + 1;
+			return (1);
+		}
+		else
+		{
+			*line = ft_strjoin(*line, buffer);
+		}
+	}
 	return (0);
 }
