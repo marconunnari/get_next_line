950217fc480e08928362b368bcb28ccc356c9596
Marco Nunnari <mnunnari@student.42.us.org>
2017-04-22 15:25:28 +0200

    norminated

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: mnunnari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/21 17:51:47 by mnunnari          #+#    #+#             */
-/*   Updated: 2017/04/21 17:52:23 by mnunnari         ###   ########.fr       */
+/*   Updated: 2017/04/22 15:23:50 by mnunnari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int			split(char **line, char *string, char *newlineptr, char **remain)
+int				split(char **line, char *string,
+						char *newlineptr, char **remain)
 {
 	*line = ft_strjoin(*line, ft_strsub(string, 0, newlineptr - string));
 	*remain = newlineptr + 1;
@@ -27,32 +28,46 @@ void			setline(char **line, char *string)
 		*line = string;
 }
 
-int			get_next_line(const int fd, char **line)
+int				process_remain(char **remain, char **line)
+{
+	char	*newlineptr;
+
+	if (*remain)
+	{
+		if ((newlineptr = ft_strchr(*remain, '\n')))
+			return (split(line, *remain, newlineptr, remain));
+		setline(line, *remain);
+		*remain = NULL;
+	}
+	return (0);
+}
+
+int				process_fd(const int fd, char **line, char **remain)
 {
 	char			*buffer;
 	char			*newlineptr;
-	static char		*remain;
 	int				ret;
 
-	*line = "";
-	if (remain)
-	{
-		if ((newlineptr = ft_strchr(remain, '\n')))
-			return (split(line, remain, newlineptr, &remain));
-		setline(line, remain);
-		remain = NULL;
-	}
-	IFRETURN(fd < 0, -1);
 	IFRETURN(!(buffer = ft_strnew(BUFF_SIZE + 1)), -1);
 	while ((ret = read(fd, buffer, BUFF_SIZE) > 0))
 	{
 		if ((newlineptr = ft_strchr(buffer, '\n')))
-			return (split(line, buffer, newlineptr, &remain));
+			return (split(line, buffer, newlineptr, remain));
 		else
 			setline(line, buffer);
 		IFRETURN(!(buffer = ft_strnew(BUFF_SIZE + 1)), -1);
 	}
 	IFRETURN(ret == -1, -1);
-	IFRETURN(*line && !ft_strequ(*line,""), 1);
+	IFRETURN(*line && !ft_strequ(*line, ""), 1);
 	return (0);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static char		*remain;
+
+	*line = "";
+	IFRETURN(process_remain(&remain, line), 1);
+	IFRETURN(fd < 0, -1);
+	return (process_fd(fd, line, &remain));
 }
