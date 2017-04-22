f540f1770c9d81e1d3fe3f86b4839637910400aa
Marco Nunnari <mnunnari@student.42.us.org>
2017-04-22 18:26:42 +0200

    compression and management multiple file descriptors roughly

--- get_next_line.c
+++ get_next_line.c
@@ -6,40 +6,29 @@
 /*   By: mnunnari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/21 17:51:47 by mnunnari          #+#    #+#             */
-/*   Updated: 2017/04/22 15:23:50 by mnunnari         ###   ########.fr       */
+/*   Updated: 2017/04/22 18:22:33 by mnunnari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int				split(char **line, char *string,
+int				process_line(char **line, char *string,
 						char *newlineptr, char **remain)
 {
-	*line = ft_strjoin(*line, ft_strsub(string, 0, newlineptr - string));
-	*remain = newlineptr + 1;
-	return (1);
-}
-
-void			setline(char **line, char *string)
-{
-	if (*line)
-		*line = ft_strjoin(*line, string);
-	else
-		*line = string;
-}
-
-int				process_remain(char **remain, char **line)
-{
-	char	*newlineptr;
-
-	if (*remain)
+	if ((newlineptr = ft_strchr(string, '\n')))
 	{
-		if ((newlineptr = ft_strchr(*remain, '\n')))
-			return (split(line, *remain, newlineptr, remain));
-		setline(line, *remain);
-		*remain = NULL;
+		*line = ft_strjoin(*line, ft_strsub(string, 0, newlineptr - string));
+		*remain = newlineptr + 1;
+		return (1);
 	}
-	return (0);
+	else
+	{
+		if (*line)
+			*line = ft_strjoin(*line, string);
+		else
+			*line = string;
+		return (0);
+	}
 }
 
 int				process_fd(const int fd, char **line, char **remain)
@@ -48,13 +37,17 @@ int				process_fd(const int fd, char **line, char **remain)
 	char			*newlineptr;
 	int				ret;
 
+	*line = "";
+	newlineptr = NULL;
+	if (*remain)
+	{
+		IFRETURN(process_line(line, *remain, newlineptr, remain), 1);
+		*remain = NULL;
+	}
 	IFRETURN(!(buffer = ft_strnew(BUFF_SIZE + 1)), -1);
 	while ((ret = read(fd, buffer, BUFF_SIZE) > 0))
 	{
-		if ((newlineptr = ft_strchr(buffer, '\n')))
-			return (split(line, buffer, newlineptr, remain));
-		else
-			setline(line, buffer);
+		IFRETURN(process_line(line, buffer, newlineptr, remain), 1);
 		IFRETURN(!(buffer = ft_strnew(BUFF_SIZE + 1)), -1);
 	}
 	IFRETURN(ret == -1, -1);
@@ -64,10 +57,13 @@ int				process_fd(const int fd, char **line, char **remain)
 
 int				get_next_line(const int fd, char **line)
 {
-	static char		*remain;
+	static char		**remains;
+	char			**remain;
 
-	*line = "";
-	IFRETURN(process_remain(&remain, line), 1);
 	IFRETURN(fd < 0, -1);
-	return (process_fd(fd, line, &remain));
+	if (!remains)
+		remains = (char**)malloc(sizeof(char*) * 1024);
+	IFRETURN(!remains, -1);
+	remain = &remains[fd];
+	return (process_fd(fd, line, remain));
 }
